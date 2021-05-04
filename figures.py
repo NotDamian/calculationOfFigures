840cc4bbd0b65819bc1ac4f0c403bdf5a4212c13
Damian Suchiński <damian@MacBook-Pro-Damian.local>
2021-05-04 10:49:39 +0200

    first commit

---
+++ figures.py
@@ -0,0 +1,27 @@
+'''
+Projekt Python
+Napisać skrypt który po uruchomieniu będzie umożliwiał nam obliczenie obwodu, pola takich figur jak:
+- trójkąt
+- prostokąt
+- kwadrat
+- koło
+- trapez
+oraz wyświetlenie figur na układzie współrzędnym. Układ współrzędny musi być przedstawiony graficznie, reszta może pokazywać w terminalu.
+
+
+Przykład:
+1. Wybieramy figurę do obliczenia
+2. Podajemy dane figury A(x,y), B(x,y), C(x,y)
+3. Wysiedla się nam graficzne przestawienie figury na układzie współrzędnym
+4. Obliczamy długości potrzebne do podania obwodu i pola
+
+Powinien mieć:
+1. Menu
+	a. Trójkąt
+	b. Prostokąt
+	c. Kwadrat
+	d. Koło
+	e. Trapez
+	f. Informacje o twórcy
+	g. Wyjście
+'''
