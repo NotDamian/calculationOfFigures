1da1ef00eb6b4cd5220d962f0f2c592133b075c7
Damian Suchiński <damian@MacBook-Pro-Damian.local>
2021-05-04 11:17:34 +0200

    add field and circumference functions

--- figures.py
+++ figures.py
@@ -2,10 +2,10 @@
 Projekt Python
 Napisać skrypt który po uruchomieniu będzie umożliwiał nam obliczenie obwodu, pola takich figur jak:
 - trójkąt
-- prostokąt
-- kwadrat
-- koło
-- trapez
+- prostokąt kk
+- kwadrat kk
+- koło kk
+- trapez kk
 oraz wyświetlenie figur na układzie współrzędnym. Układ współrzędny musi być przedstawiony graficznie, reszta może pokazywać w terminalu.
 
 
@@ -25,3 +25,28 @@ Powinien mieć:
 	f. Informacje o twórcy
 	g. Wyjście
 '''
+
+def circleField(r):
+	return (3.14 * r * r)
+def circumferenceOfACircle(r):
+	return (2 * 3.14 * r)
+
+def ctrapezeField(a,b,h):
+	return ((a + b) * h) / 2
+def circumferenceOfATrapeze(a, b, c, d):
+	return (a + b + c + d)
+
+def squareField(a):
+	return a * a
+def circumferenceOfASquare(a):
+	return a * 4
+
+def rectangleField(a, b):
+	return a * b
+def circumferenceOfARectangle(a, b):
+	return (2 * a) + (2 * b)
+
+def triangleField(a, h):
+	return (a * h) / 2
+def circumferenceOfATriangle(a, b, c):
+	return a + b + c
